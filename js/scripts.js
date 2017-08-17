443e2ea48145fe62e60dcf3534aca70d355bf7a3
Dan Nollette and snow vilay <snow@>
2017-08-17 14:07:38 -0700

    Add downcasing in frontend and special character deletion in backend.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 //Business Logic(Back-End)
-
+function cryto(text){
+  //var alphabet = /[a-z]/;
+  var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var characters = text.split("");
+  var onlyLetters = [];
+  characters.forEach(function(character){
+    alphabet.forEach(function(letter){
+      if(character === letter){
+        onlyLetters.push(character);
+      }
+    });
+  });
+  alert(onlyLetters);
+};
 
 
 
@@ -16,5 +29,11 @@
 
 //User Interface (UI or Front-End)
 $(document).ready(function(){
+  $("#crytosquare-form").submit(function(event){
+    var textInput = $("#text-input").val().toLowerCase();
+    var output = cryto(textInput);
 
+    $("#output").text(output);
+    event.preventDefault();
+  })
 });
